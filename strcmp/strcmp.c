6a3f281db3ab4743ee2ce0b82947c208d273dd44
dylanabzr <dylanab@pm.me>
2023-09-26 17:56:03 -0300

    Correcting bugs

--- strcmp/strcmp.c
+++ strcmp/strcmp.c
@@ -1,5 +1,5 @@
 int my_strcmp(const char* str1, const char* str2){
-  while(*str1 != '\0'){
+  while((*str1 != '\0') || (*str2 != '\0') ){
     if(*str1 > *str2) return 1;
     if(*str1 < *str2) return -1;
     str1++;
